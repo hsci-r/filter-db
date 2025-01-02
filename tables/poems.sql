a66b87e9f5cd769d7f56b99df9de89749ab3fa21
Maciej Janicki <maciej.janicki@helsinki.fi>
2025-01-02 19:29:45 +0200

    Fix the name of the poems table
    Got accidentally changed in 7e16e081.

--- tables/poems.sql
+++ tables/poems.sql
@@ -1,8 +1,8 @@
 INSERT INTO dbmeta VALUES ('poems', current_timestamp(), 0)
   ON DUPLICATE KEY UPDATE last_modified = current_timestamp(), ready = 0;
 
-DROP TABLE IF EXISTS poems_test;
-CREATE TABLE poems_test(
+DROP TABLE IF EXISTS poems;
+CREATE TABLE poems(
   p_id INTEGER NOT NULL,
   nro CHAR(16),
   collection CHAR(8) DEFAULT NULL,
@@ -13,10 +13,10 @@ CREATE TABLE poems_test(
   KEY(display_name)
 ) CHARACTER SET 'utf8';
 
-LOAD DATA LOCAL INFILE 'data/poems.tbl.csv' INTO TABLE poems_test
+LOAD DATA LOCAL INFILE 'data/poems.tbl.csv' INTO TABLE poems
   FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 ROWS;
 
-UPDATE poems_test SET display_name = nro WHERE display_name IS NULL;
+UPDATE poems SET display_name = nro WHERE display_name IS NULL;
 
 UPDATE dbmeta SET last_modified = current_timestamp(), ready = 1
   WHERE table_name = 'poems';
